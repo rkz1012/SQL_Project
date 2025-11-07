67b02c84f67ed43ba87ec9b6028189a78df9e597
Rkz1012 <rajkumaar.rk.1999@gmail.com>
2025-11-07 16:08:56 +0530

    all months

--- test.sql
+++ test.sql
@@ -5,3 +5,69 @@ CREATE Table january_jobs AS
     FROM 
       job_postings_fact
     where (EXTRACT(MONTH FROM job_posted_date) = 1);
+
+    -- February (Month 2)
+CREATE TABLE february_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 2;
+
+-- March (Month 3)
+CREATE TABLE march_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 3;
+
+-- April (Month 4)
+CREATE TABLE april_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 4;
+
+-- May (Month 5)
+CREATE TABLE may_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 5;
+
+-- June (Month 6)
+CREATE TABLE june_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 6;
+
+-- July (Month 7)
+CREATE TABLE july_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 7;
+
+-- August (Month 8)
+CREATE TABLE august_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 8;
+
+-- September (Month 9)
+CREATE TABLE september_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 9;
+
+-- October (Month 10)
+CREATE TABLE october_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 10;
+
+-- November (Month 11)
+CREATE TABLE november_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 11;
+
+-- December (Month 12)
+CREATE TABLE december_jobs AS
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 12;
