ce2c8f0d654b163bc242f08846699357a3922016
Rkz1012 <rajkumaar.rk.1999@gmail.com>
2025-11-10 13:24:02 +0530

    case prob

--- CASE_Statemennts.sql
+++ CASE_Statemennts.sql
@@ -7,3 +7,25 @@ Select
     END AS location_category
 FROM job_postings_fact
 GROUP BY location_category;
+
+
+--problem
+
+select job_id, job_title, job_location,
+    CASE
+        When salary_year_avg > 250000 THEN 'High Salary'
+        WHEN salary_year_avg BETWEEN 100000 AND 250000 THEN 'Standard Salary'
+        WHEN salary_year_avg < 100000 THEN 'Low Salary'
+        ELSE 'Not Disclosed'
+    END AS salary_category
+
+FROM
+    job_postings_fact
+WHERE
+    salary_year_avg is not NULL AND job_title_short = 'Data Analyst';
+ORDER BY salary_category;
+
+
+select * from job_postings_fact where
+ (salary_year_avg is not NULL) AND job_title_short = 'Data Analyst'
+ORDER BY salary_year_avg desc limit 10;
