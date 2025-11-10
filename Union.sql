bc8d5ecdd4d51fc89c6dc7bc918727f9f881a1da
Rkz1012 <rajkumaar.rk.1999@gmail.com>
2025-11-10 15:18:08 +0530

    practicve prob union

--- Union.sql
+++ Union.sql
@@ -31,3 +31,25 @@ SELECT
     job_location
 FROM 
     february_jobs;
+
+
+--- Practice problem
+SELECT 
+    q1_job_postings.job_title_short,
+    q1_job_postings.job_location,
+    q1_job_postings.job_via,
+    q1_job_postings.salary_year_avg,
+    q1_job_postings.job_posted_date :: date
+FROM
+(
+select *
+from january_jobs
+UNION ALL
+select *
+from february_jobs
+UNION ALL
+select *
+from march_jobs
+) as q1_job_postings
+where
+ q1_job_postings.salary_year_avg > 70000 and q1_job_postings.job_title_short = 'Data Analyst';
