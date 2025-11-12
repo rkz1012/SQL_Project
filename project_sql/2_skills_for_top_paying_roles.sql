be3cfafce4a70bda1c3b8cbc4914ff86351ba0c2
Rkz1012 <rajkumaar.rk.1999@gmail.com>
2025-11-12 12:49:57 +0530

    qq2

--- project_sql/2_skills_for_top_paying_roles.sql
+++ project_sql/2_skills_for_top_paying_roles.sql
@@ -6,22 +6,27 @@ Question: What skills are required for the top-paying data analyst jobs?
 helping job seekers understand which skills to develop that align with top salaries
 */
 
+WITH top_paying_jobs AS ( 
+    SELECT
+        job_id,
+        job_title,
+        name as company_name,
+        salary_year_avg
+    FROM
+        job_postings_fact  
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short LIKE '%Data Analyst%'
+        AND job_location = 'Anywhere'
+        AND salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
 
-SELECT
-    job_id,
-    job_title,
-    name as company_name,
-    job_location,
-    job_schedule_type,
-    salary_year_avg,
-    job_posted_date
-FROM
-    job_postings_fact  
-LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
-WHERE
-    job_title_short LIKE '%Data Analyst%'
-    AND job_location = 'Anywhere'
-    AND salary_year_avg IS NOT NULL
-ORDER BY
-    salary_year_avg DESC
-LIMIT 10;
+SELECT 
+    top_paying_jobs.* ,
+    skills_dim.skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim on top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
