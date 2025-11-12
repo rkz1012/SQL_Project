bf4dec2a9f39e86d42ee9acaaf8d19ce5d5641de
Rkz1012 <rajkumaar.rk.1999@gmail.com>
2025-11-12 13:22:32 +0530

    q3 complete

--- project_sql/3_most_in_demand_skills_for_DA.sql
+++ project_sql/3_most_in_demand_skills_for_DA.sql
@@ -16,6 +16,9 @@ SELECT
 FROM job_postings_fact
 INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short LIKE '%Data Analyst%' AND
+    job_work_from_home = TRUE
 GROUP BY
     skills
 ORDER BY
