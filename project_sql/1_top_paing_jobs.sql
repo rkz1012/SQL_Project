284d306e5f78a16ad0ccad10eae2334dff3fa3d3
Rkz1012 <rajkumaar.rk.1999@gmail.com>
2025-11-12 12:23:37 +0530

    q1 done

--- project_sql/1_top_paing_jobs.sql
+++ project_sql/1_top_paing_jobs.sql
@@ -5,3 +5,21 @@ Question: What are the top-paying data analyst jobs?
 - Why? Highlight the top-paying opportunities for Data Analysts, offering insights into emp
 */
 
+SELECT
+    job_id,
+    job_title,
+    name as company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact  
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short LIKE '%Data Analyst%'
+    AND job_location = 'Anywhere'
+    AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
