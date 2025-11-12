d46e072d01e15302a480ca05ceb00d062a559fca
Rkz1012 <rajkumaar.rk.1999@gmail.com>
2025-11-12 13:07:39 +0530

    edit

--- project_sql/3_most_in_demand_skills_for_DA.sql
+++ project_sql/3_most_in_demand_skills_for_DA.sql
@@ -7,3 +7,10 @@ Question: What are the most in-demand skills for data analysts?
 - Why? Retrieves the top 5 skills with the highest demand in the job market,
   💡 providing insights into the most valuable skills for job seekers.
 ⚡*/
+
+
+
+SELECT *
+FROM job_postings_fact
+INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
