4d0228ff224ae164f3ce4a7263b107a1903011ba
Rkz1012 <rajkumaar.rk.1999@gmail.com>
2025-11-12 13:20:35 +0530

    qq3

--- project_sql/3_most_in_demand_skills_for_DA.sql
+++ project_sql/3_most_in_demand_skills_for_DA.sql
@@ -10,7 +10,14 @@ Question: What are the most in-demand skills for data analysts?
 
 
 
-SELECT *
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) as demand_count
 FROM job_postings_fact
 INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+Limit 5;
